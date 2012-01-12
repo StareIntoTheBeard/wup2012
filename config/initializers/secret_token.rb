08ed15e7461781c748ed077438c4ebd835422bca
Dan Schuman <danschuman@gmail.com>
2012-01-12 15:27:21 -0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wup2012::Application.config.secret_token = '98e0988c5b34f3477f237e14c78695d2408a4663fbc91229e363067a44f721efd7059a4cc61b9c8af2bb0756b3cf6ec2b7f4265cb9b1f9b2b2fdeec3b66b0fa7'
