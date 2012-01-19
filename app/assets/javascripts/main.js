4798b5f3068fb03eaf85169030ad07488674e84d
Dan Schuman <danschuman@gmail.com>
2012-01-19 13:42:18 -0800

    Home page contact link

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+function animate_footer() {
+	$('footer').animate({height: '40px', 'font-size': '16px', 'padding-top': '10px'}, 500);
+}
+
+$(function(){
+	$('#contact_link').click(animate_footer);
+});
