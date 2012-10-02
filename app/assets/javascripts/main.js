fd23f6434a56633d8d80d1fa253719cee140c335
justinwup <justin@winduppixel.com>
2012-10-02 12:16:41 -0500

    hey

--- app/assets/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-// function animate_footer() {
-// 	$('footer').animate({height: '40px', 'font-size': '16px', 'padding-top': '10px'}, 500);
-// }
-
-// $(function(){
-// 	// $('#contact_link').click(animate_footer);
-// });
